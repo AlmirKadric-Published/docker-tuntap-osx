0ba51df5072ecfad178108b66926ad9691b822c3
Robert Elliot <rob@lidalia.org.uk>
2020-12-16 11:15:32 +0000

    Formatting

--- sbin/docker_tap_up_routes.sh
+++ sbin/docker_tap_up_routes.sh
@@ -23,24 +23,35 @@ add_route_for() {
 
 add_routes_for_new_networks() {
 	# TODO need to remove routes on 'event=destroy'
-	# Because the network has been destroyed, `docker network inspect` will not work.
+	# Because the network has been destroyed, `docker network inspect` won't work.
 	# Options:
-	# 1) Maintain state somewhere (ID to subnet) when we add the route, so that on event=destroy we can look up the subnet and remove the route
-	# 2) Synchronize routes on each event - find all routes to tap1, find all bridge networks, add and delete as appropriate
-	docker events --filter 'type=network' --filter 'event=create' --format '{{index .Actor.Attributes "type"}} {{.ID}}' | \
-		while read -r event; do
-			IFS=' ' read -r type network <<< "$event"
-			if [ "$type" = "bridge" ]; then
-				add_route_for "$network"
-			fi
-		done
+	# 1) Maintain state somewhere (ID to subnet) when we add the route, so that on
+	#    event=destroy we can look up the subnet and remove the route
+	# 2) Synchronize routes on each event - find all routes to tap1, find all
+	#    bridge networks, add and delete as appropriate
+	docker events \
+	  --filter 'type=network' \
+	  --filter 'event=create' \
+	  --format '{{index .Actor.Attributes "type"}} {{.Actor.ID}}' \
+		  | while read -r event; do
+					IFS=' ' read -r type network <<< "$event"
+					if [ "$type" = "bridge" ]; then
+						add_route_for "$network"
+					fi
+				done
+}
+
+get_all_networks() {
+	docker network ls \
+	  --filter 'driver=bridge' \
+	  --format '{{.ID}}'
 }
 
 main() {
 
 	add_routes_for_new_networks &
 
-  local networks; networks=$(docker network ls --filter driver=bridge --format '{{.ID}}')
+  local networks; networks=$(get_all_networks)
   for network in $networks; do
   	add_route_for "$network"
 	done
